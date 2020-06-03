e94a12a45306e21dc8adc6bbf595a0e508a22092
duyanh1992 <alyssachia1992@gmail.com>
2020-06-03 16:06:35 +0700

    basic remove todolist item

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import ColorBox from './components/BoxColor';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [totoList, setTodoList] = useState([
+    { id: 1, name: 'Reading books' },
+    { id: 2, name: 'Coding React Js' },
+    { id: 3, name: 'Going for a walk' }
+  ]);
+
+  function handleTodoClick(todoId) {
+    const index = totoList.findIndex(todo => todo.id === todoId);
+
+    const newState = [...totoList];
+    newState.splice(index, 1);
+    setTodoList(newState);
+  }
+
   return (
     <div className="app">
       <h1>React hooks basic</h1>
 
-      <ColorBox />
+      {/* <ColorBox />*/}
+
+      <TodoList todos={totoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
